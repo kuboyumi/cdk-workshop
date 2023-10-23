e4f5dbab1158b599b437b429f567cfb7b2fa9862
kuboyumi <121540707+kuboyumi@users.noreply.github.com>
2023-10-24 01:51:33 +0900

    fix: hitcounter.ts

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -1,4 +1,5 @@
 import { 
+  aws_dynamodb,
   aws_lambda,
 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
@@ -9,9 +10,25 @@ export interface HitCounterProps {
 }
 
 export class HitCounter extends Construct {
+
+  /** allows accessing the counter function */
+  public readonly handler: aws_lambda.Function;
+
   constructor(scope: Construct, id: string, props: HitCounterProps) {
     super(scope, id);
 
-    // TODO
+    const table = new aws_dynamodb.Table(this, 'Hits', {
+      partitionKey: { name: 'path', type: aws_dynamodb.AttributeType.STRING}
+    });
+
+    this.handler = new aws_lambda.Function(this, 'HitCounterHandler', {
+      runtime: aws_lambda.Runtime.NODEJS_14_X,
+      handler: 'hitcounter.handler',
+      code: aws_lambda.Code.fromAsset('lambda'),
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName
+      }
+    });
   }
 }
